511ce81ac5917b2ccfa249b06538d91ce08c9f08
Katherine Bellman <katherine.bellman@dcmail.ca>
2021-11-25 22:20:52 -0500

    Updated comments header information

--- src/ca/durhamcollege/RandomVector.java
+++ src/ca/durhamcollege/RandomVector.java
@@ -1,8 +1,8 @@
 
 /**
  * @authors  Katherine Bellman - 100325825
- * @file    OOP3200 - Fall 2021 - Java ICE 9
- * @date    November 23rd, 2021
+ * @file    OOP3200 - Fall 2021 - Java ICE 10
+ * @date    November 24th 2021
  */
 
 package ca.durhamcollege;
